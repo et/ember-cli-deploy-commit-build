46a984e3656912dfd9d0291e79ec20fabf8f3e99
Eric Thomas <eric.thomas@salesforce.com>
2016-01-10 10:20:47 -0700

    configure plugin

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 /* jshint node: true */
 'use strict';
 
+var BasePlugin = require('ember-cli-deploy-plugin');
+
 module.exports = {
-  name: 'ember-cli-deploy-commit-build'
+  name: 'ember-cli-deploy-commit-build',
+
+  createDeployPlugin: function(options) {
+    var DeployPlugin = BasePlugin.extend({
+      name: options.name,
+
+      didBuild: function(context) {
+        this.log("DID BUILD");
+        this.log(context);
+      }
+    });
+
+    return new DeployPlugin();
+  }
 };
